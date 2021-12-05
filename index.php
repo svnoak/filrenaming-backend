beed5b1f0344df427e13f28e6573fe996a875716
Kim <kimpfimp@posteo.net>
2021-12-05 19:03:21 +0100

    barebone functionality incoming

--- index.php
+++ index.php
@@ -1,22 +1,57 @@
 <?php
-
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
     // Tillåt alla (origins) och alla headers
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: *");
     exit();
-} 
-
+}
 header("Access-Control-Allow-Origin: *");
 
+    if( $_SERVER["REQUEST_METHOD"] == "POST" ){
 
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $files = getJSON("php://input");
+    $upload = $_POST;
+    $files = $_FILES;
 
-    sendJSON(["data"=>$files], 200);
-} else {
-    sendJSON(["message"=>"INVALID REQUEST"], 400);
-}
+    //$folderName = uniqid();
+    $zip = new ZipArchive();
+    $zip_name = uniqid() . ".zip";
+    $path_to_zip = "zip/$zip_name";
+    $zip->open("$path_to_zip", ZIPARCHIVE::CREATE);
+    foreach( $files as $index=>$file ){
+        $tmp_names = $file["tmp_name"];
+        $name = $upload[$index];
+        $error = $file["error"];
+        //mkdir("./uploads/$folderName");
+        //moveImages($tmp_names, $name, $error, $folderName);
+        for( $i = 0; $i < count($tmp_names); $i++ ){
+            $err = $error[$i];
+            if( $err === 0 ){
+                $number = $i+1;
+                $tmp_name = $tmp_names[$i];
+                $imgName = "$name-$number.png";
+                $zip->addFile($tmp_name, $imgName);
+                //move_uploaded_file($tmp_name, "uploads/$folderName/$imgName.png");
+            } else {
+                //sendJSON(["message"=>"One or more images failed uploading, please try again"], 400);
+                //exit();
+            }
+        }
+    }
+    $zip->close();
+        if (is_readable($path_to_zip)) {
+            $path = $_SERVER["DOCUMENT_ROOT"];
+            sendJSON(["value"=>"http://localhost:7000/" . "$path_to_zip", "message"=>"Success"], 200);
+            exit();
+        }
+        else{
+            sendJSON(["message"=>"Something went wrong"], 400);
+            exit();
+        }
+    }
+    elseif ($_SERVER["REQUEST_METHOD"] == "GET"){
+        sendJSON(["message"=>"DOWNLOAD STARTED"], 200);
+    }
+
 
 function sendJSON($message, $statusCode) {
     //is used whenever something is successful 
@@ -27,9 +62,4 @@ function sendJSON($message, $statusCode) {
     $jsonMessage = json_encode($message);
 
     echo($jsonMessage);
-    exit();
-}
-
-function getJSON($filename){
-    return json_decode(file_get_contents($filename), true);
 }
